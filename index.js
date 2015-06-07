bd61f83346a5128a99ebd1c6ef0bef94ab9a59ef
Jon Levy <nonni@nonni.cc>
2015-06-07 20:26:16 +0000

    stuff

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var ds18b20 = require('ds18b20');
+var Datastore = require('nedb');
 
+db = {}
+db.temperature = new Datastore('temperature.db');
+db.temperature.loadDatabase();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,11 +19,11 @@ io.on('connection', function(socket){
     console.log('temperature: ' + temperature);
   });
 
-var mySensors = ds18b20.sensors(function(err, ids) {
-  if (err) {
+  var mySensors = ds18b20.sensors(function(err, ids) {
+    if (err) {
       console.log('Oops, something bad happened!');
       return;
-  }
+    }
     setInterval(function(){
         ids.forEach(function(item) {
             ds18b20.temperature(item, function(err, value) {
@@ -28,7 +32,13 @@ var mySensors = ds18b20.sensors(function(err, ids) {
                   return;
               }
               //console.log('Current temperature is', value);
-		socket.emit('temperature', {'temperature': value});
+                var tempData = { 'sensorId': item, 'date': Date.now(), 'temperature': value  };
+                db.temperature.insert(tempData, function (err, newDocs) {
+                console.log(tempData);
+        // newDocs is an array with these documents, augmented with their _id
+        });
+
+        socket.emit('temperature', {'temperature': value});
            });
         });
 
