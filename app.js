2c964e5ad7e59f0d440a366e34611e176a70a5d0
Jón Levy <nonni@nonni.cc>
2015-06-08 16:00:09 +0000

    run script if on Pi

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express  = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var ds18b20 = require('ds18b20');
+//var ds18b20 = require('ds18b20');
 var Datastore = require('nedb');
 var path = require('path');
+
+var soilSensor = '28-000006a3684e'
+var airSensor = '28-000006a36dbe'
+
+var os = require("os");
 
 db = {}
 db.temperature = new Datastore('temperature.db');
@@ -16,43 +21,44 @@ app.get('/', function(req, res){
 });
 
 
-/*
-var mySensors = ds18b20.sensors(function(err, ids) {
-  if (err) {
-    console.log('Oops, something bad happened!');
-    return;
-  }
+if (os.hostname() === 'raspberrypi') {
+  var mySensors = ds18b20.sensors(function(err, ids) {
+    if (err) {
+      console.log('Oops, something bad happened!');
+      return;
+    }
 
-  console.log(ids);
+    console.log(ids);
 
-  setInterval(function(){
-      ids.forEach(function(item) {
-          ds18b20.temperature(item, function(err, value) {
-            if (err) {
-                console.log('Oops, something bad happened!');
-                return;
-            }
-            //console.log('Current temperature is', value);
-              var tempData = { 'sensorId': item, 'date': Date.now(), 'temperature': value  };
-              db.temperature.insert(tempData, function (err, newDocs) {
-              console.log(tempData);
-      // newDocs is an array with these documents, augmented with their _id
-      });
-      
-         });
-      });
-  }, 5000);
-});
-*/
+    setInterval(function(){
+        ids.forEach(function(item) {
+            ds18b20.temperature(item, function(err, value) {
+              if (err) {
+                  console.log('Oops, something bad happened!');
+                  return;
+              }
+              //console.log('Current temperature is', value);
+                var tempData = { 'sensorId': item, 'date': Date.now(), 'temperature': value  };
+                db.temperature.insert(tempData, function (err, newDocs) {
+                console.log(tempData);
+        // newDocs is an array with these documents, augmented with their _id
+        });
+        
+           });
+        });
+    }, 5000);
+  });
+}
 
 io.on('connection', function(socket){
   console.log('a user connected');
   
   setInterval(function(){
-    db.temperature.find({}).sort({date: -1}).limit(1).exec(function (err, docs) {
-      console.log('Docs: '+docs[0].temperature);
+    db.temperature.find({sensorId: { $in: [soilSensor, airSensor]}}).sort({date: -1}).limit(2).exec(function (err, docs) {
+      console.log(docs[0].sensorId+': '+docs[0].temperature);
+      console.log(docs[1].sensorId+': '+docs[1].temperature);
       console.log('Date: '+ new Date(docs[0].date * 1000).toLocaleString());
-      //socket.emit('temperature', docs[0]);
+      socket.emit('temperature', docs);
     });
   }, 5000);
 
@@ -63,6 +69,6 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:8000');
+http.listen(8000, function(){
+  console.log('listening on 0:8000');
 });
