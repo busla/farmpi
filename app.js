16b5193443c421affc0aea51add979be1981bd4e
Jón Levy <nonni@nonni.cc>
2015-06-09 01:09:27 +0000

    massive update

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ var io = require('socket.io')(http);
 var Datastore = require('nedb');
 var path = require('path');
 var _ = require('underscore');
+var fs = require('fs');
 
 var sensors = [
   {
     'id': '28-000006a3684e',
+    'type': 'soil',
     'name': 'Soil sensor',
   },  
   {
     'id': '28-000006a36dbe',
+    'type': 'air',
     'name': 'Air sensor',
   }  
 ]
@@ -26,15 +29,24 @@ db = {}
 db.temperature = new Datastore('temperature.db');
 db.temperature.loadDatabase();
 
-app.use(express.static(path.join(__dirname, 'ui')));
+app.use(express.static('data'));
+app.use(express.static('ui'));
+app.use(express.static('bower_components'));
+
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/ui/index.html');
+});
+
+app.get('/data/charts.json', function(req, res){
+  res.sendFile(__dirname + '/data/charts.json');
 });
 
 console.log('Hostname: '+hostname)
 
 /**
-  Read the sensors and save to DB if running on the Pi.
+  If on the Pi, read the sensors and save to DB.
 **/
 
 if (hostname === 'raspberrypi') { 
@@ -70,21 +82,60 @@ if (hostname === 'raspberrypi') {
 }
 
 
-setInterval(function(){
-
-  db.temperature.find({sensorId: { $in: _.pluck(sensors, 'id')}}).sort({date: -1}).limit(2).exec(function (err, docs) {
-    docs.forEach(function(item) {
-      
+function prepareData(item) {
       var tempDate = new Date(item.date);
-      
-
       sensor = _.findWhere(sensors, {'id': item.sensorId})
 
+      data = {
+        'date': tempDate,
+        'temperature': item.temperature,
+        'name': sensor.name,
+        'type': sensor.type,
+      };
+      return data;  
+}
+
+
+var allData = function dumpData() {
+  var chartData = []
+
+  db.temperature.find({}).sort({date: 1}).exec(function (err, docs) {
+    
+    docs.forEach(function(item) {      
+      data = prepareData(item);
+      chartData.push(data);
+    });
+    var str = JSON.stringify(chartData);
+
+    fs.writeFile("data/charts.json", str, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });     
+  });  
+  
+}
+
+allData();
+
+setInterval(function(){
+  var query = {};
+  var r = _.random(0, 1000);
+  
+  //db.temperature.find({sensorId: { $in: _.pluck(sensors, 'id')}}).sort({date: -1}).limit(2).exec(function (err, docs) {
+  db.temperature.find(query).limit(1).skip(r).exec(function (err, docs) {
+    docs.forEach(function(item) {
+
+      data = prepareData(item);
+
       console.log('#################################');
-      console.log("Sensor: " + sensor.name);
-      console.log("Temperature: "+item.temperature);
-      console.log("Date: "+ tempDate);
-      currentTemp = item;
+      console.log("Sensor: " + data.name);
+      console.log("Type: " + data.type);
+      console.log("Temperature: " + data.temperature);
+      console.log("Date: "+ data.date);      
+      currentTemp = data;
     });      
   });    
 
@@ -92,7 +143,6 @@ setInterval(function(){
   
 io.on('connection', function(socket){
   console.log('a user connected');
-
   setInterval(function(){
     io.emit('temperature', currentTemp);
   }, 1000);
