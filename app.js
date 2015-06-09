659a4a3f914a6ef5f557e1ce5c97f2e26713bd80
Jon Levy <nonni@nonni.cc>
2015-06-09 09:19:15 +0000

    change port

--- app.js
+++ app.js
@@ -154,6 +154,6 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(8000, function(){
-  console.log('listening on 0:8000');
+http.listen(3000, function(){
+  console.log('listening on 0:3000');
 });
